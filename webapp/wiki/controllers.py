b0b4c544422066362f4a178e865ff8c6170977ad
HealYouDown <jeremyregitz@gmail.com>
2019-05-29 21:57:38 +0200

    removed wiki idea

--- webapp/wiki/controllers.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-wiki = Blueprint("wiki", __name__, url_prefix="/wiki")
-
-@wiki.route("/")
-def index():
-    return render_template("wiki/index.html", title="Wiki")
