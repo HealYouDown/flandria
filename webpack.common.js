f13ba8b1fb15ce378eb30c6b573c693dfe4bd8df
HealYouDown <jeremyregitz@gmail.com>
2019-08-05 01:44:52 +0200

    added cleaning before every build

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,24 @@
+var WebpackBeforeBuildPlugin = require('before-build-webpack');
+const path = require('path');
+const fs = require('fs')
+
 module.exports = {
   entry: {
     app: "./src/index.js"
   },
+  plugins: [
+    new WebpackBeforeBuildPlugin(function(stats, callback) {
+      // Clears output folder from all previous things except folders
+      const outputDir = __dirname + "/app/static";
+      fs.readdirSync(outputDir).forEach(filename => {
+        const filenamePath = path.join(outputDir, filename)
+        if (!fs.lstatSync(filenamePath).isDirectory()) {
+          fs.unlinkSync(filenamePath);
+        }
+      });
+      callback();
+    }),
+  ],
   output: {
     path: __dirname + "/app/static",
     filename: "[hash].bundle.js",
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CompressionPlugin = require('compression-webpack-plugin');
-const path = require('path');
-const fs = require('fs');
-
-// Clears output folder from all previous things except folders
-const outputDir = common.output.path;
-fs.readdirSync(outputDir).forEach(filename => {
-  const filenamePath = path.join(outputDir, filename)
-  if (!fs.lstatSync(filenamePath).isDirectory()) {
-    fs.unlinkSync(filenamePath);
-  }
-});
 
 module.exports = merge(common, {
   mode: "production",
