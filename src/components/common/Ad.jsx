e88faad90c21ed1acbdeb369c4b60a094919d5e0
HealYouDown <jeremyregitz@gmail.com>
2020-03-27 16:42:20 +0100

    (hopefully) fixed ad

--- src/components/common/Ad.jsx
+++ src/components/common/Ad.jsx
@@ -12,7 +12,7 @@ export default class Ad extends React.Component {
   render() {
     return (
       <ins className='adsbygoogle'
-        style={{ display: 'inline-block', width: "100%" }}
+        style={{ display: 'inline-block', minWidth: "400px", width: "100%", height: "100px", marginTop: "15px" }}
         data-ad-client='ca-pub-7852193310298972'
         data-ad-slot={this.props.slot}
       />
