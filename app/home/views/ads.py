d3006b7220786e97244667090f3fa9478ad40c73
HealYouDown <jeremyregitz@gmail.com>
2020-04-09 12:24:10 +0200

    hopefully fixed ads.txt

--- app/home/views/ads.py
+++ app/home/views/ads.py
@@ -1,6 +1,9 @@
+from flask import send_from_directory, current_app, send_file
 from app.home.blueprint import home_bp
+import os
 
 
-@home_bp.route("/ads.txt")
+@home_bp.route("/ads.txt", methods=["GET"])
 def ads_txt():
-    return "google.com, pub-7852193310298972, DIRECT, f08c47fec0942fa0"
+    path = os.path.join(current_app.static_folder, "files")
+    return send_from_directory(path, "ads.txt")
