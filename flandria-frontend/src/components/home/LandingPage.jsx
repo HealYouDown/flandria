f0c0dc11243bec1d30c9e8ace64b44b3b49d9ef3
HealYouDown <jeremyregitz@gmail.com>
2020-12-29 12:35:41 +0100

    dark mode text opacity on landing page

--- flandria-frontend/src/components/home/LandingPage.jsx
+++ flandria-frontend/src/components/home/LandingPage.jsx
@@ -7,9 +7,9 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-y-10 lg:gap-20 lg:flex-row">
       <div className="max-w-3xl">
-        <span className="text-2xl tracking-wide text-gray-500 dark:text-white">Your Florensia Database</span>
+        <span className="text-2xl tracking-wide text-gray-500 dark:text-white dark:text-opacity-90">Your Florensia Database</span>
         <h2 className="mt-3 mb-5 text-5xl font-semibold tracking-tight text-gray-700 lg:text-8xl dark:text-white">Flandria</h2>
-        <p className="max-w-lg text-base font-thin leading-7 text-gray-500 dark:text-white">
+        <p className="max-w-lg text-base font-thin leading-7 text-gray-500 dark:text-white dark:text-opacity-70">
           Flandria is an open-source database website for the MMORPG Florensia.
           It provides a clean interface to view all available monsters, items and quest in the
           world of Florensia.
